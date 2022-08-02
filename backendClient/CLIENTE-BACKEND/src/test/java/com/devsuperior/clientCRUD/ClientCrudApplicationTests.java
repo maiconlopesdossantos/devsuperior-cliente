ad7cb8651613fe26c5a667efe4af9572babaf11c
maicon.lopes <maicon.lopes@agi.com.br>
2022-08-02 10:38:29 -0300

    backend client created

---
+++ backendClient/CLIENTE-BACKEND/src/test/java/com/devsuperior/clientCRUD/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
